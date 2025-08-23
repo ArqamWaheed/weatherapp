1f615c38191e35b4b48fe195834b28483b34ee11
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-23 16:01:14 +0500

    Refactored the API into async await

--- src/core/weatherAPI.js
+++ src/core/weatherAPI.js
@@ -1,26 +1,21 @@
 const API_key = "KG6VNQ6N9DLV5J5HGB4E4WAKX";
 
-function getWeatherData(cityName) {
-  return fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}/?key=${API_key}`,
-    {
-      mode: "cors",
-    },
-  )
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error("Sybau");
-      }
-      return response.json();
-    })
-    .then((data) => data)
-    .catch((error) => {
-      throw new Error(error);
-    });
+async function getWeatherData(cityName) {
+  try {
+    let response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}/?key=${API_key}`,
+      { mode: "cors" },
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("Failed to fetch weather!", err);
+    return null;
+  }
 }
 
-getWeatherData("islamabad")
-  .then((data) => console.log(data))
-  .catch((error) => {
-    console.log("Found an error", error);
-  });
+let cityData = await getWeatherData("Islamabad");
+console.log(cityData);
